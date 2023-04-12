e5c57f8329df434c678ac4264acfee56a27d9389
ayishasana <sanaayisha1234@gmail.com>
2023-04-12 11:45:43 +0530

    OHS

---
+++ new_app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from new_app import models
+
+# Register your models here.
+admin.site.register(models.register)
+admin.site.register(models.Login)
+admin.site.register(models.complaints)
+admin.site.register(models.register1)
+admin.site.register(models.Schedule_add)
+admin.site.register(models.Take_appointment)
+admin.site.register(models.Bill)
+admin.site.register(models.CreditCard)
+
+
